03cd7e803620b5052cfece770742dac4f85e0bb4
loaloaa <loaloaaaaed@gmail.com>
2023-03-27 17:55:07 +0300

    first commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,6 +14,7 @@
     <!-- Styles -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 
+
     <style>
         body {
             font-family: 'Lato';
@@ -47,20 +48,23 @@
                 <div class="panel-body">
                     <!-- Display Validation Errors -->
                     <!-- New Task Form -->
-                    <form action="#" method="POST" class="form-horizontal">
+                    <form action="insert" method="get" class="form-horizontal">
 
                         <!-- Task Name -->
                         <div class="form-group">
                             <label for="task-name" class="col-sm-3 control-label">Task</label>
 
                             <div class="col-sm-6">
+                                @csrf
                                 <input type="text" name="name" id="task-name" class="form-control" value="">
                             </div>
                         </div>
 
                         <!-- Add Task Button -->
                         <div class="form-group">
+                            @csrf
                             <div class="col-sm-offset-3 col-sm-6">
+
                                 <button type="submit" class="btn btn-default">
                                     <i class="fa fa-btn fa-plus"></i>Add Task
                                 </button>
@@ -80,25 +84,36 @@
                         <table class="table table-striped task-table">
                             <thead>
                                 <th>Task</th>
+                                <th>Created At</th>
                                 <th>&nbsp;</th>
                             </thead>
                             <tbody>
-                                @foreach ($orders as $order)
-                                <tr>
-                                    <td class="table-text"><div>{{$order->name}}</div></td>
-
-                                    <!-- Task Delete Button -->
-                                    <td>
-                                        <form action="#" method="POST">
-                                            <button type="submit" class="btn btn-danger">
-                                                <i class="fa fa-btn fa-trash"></i>Delete
-                                            </button>
-                                        </form>
-                                    </td>
-                                </tr>
-                                @endforeach
+                                @foreach ($orders as $item)
+                                    <tr>
+                                        <td class="table-text"><div>{{$item->name}}</div></td>
 
 
+                                        <td class="table-text"><div>{{$item->created_at}}</div></td>
+
+                                        <!-- Task Delete Button -->
+                                        <td>
+                                            <form action="delete/{{$item->id}}" method="POST">
+                                                @csrf
+                                                <button type="submit" class="btn btn-danger">
+                                                    <i class="fa fa-btn fa-trash"></i>Delete
+                                                </button>
+                                            </form>
+                                        </td>
+                                        <td>
+                                            <form action="editt/{{$item->id}}" method="POST">
+                                                @csrf
+                                                <button type="submit" class="btn btn-info">
+                                                    <i class="fa fa-btn fa-info"></i>Edit
+                                                </button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                    @endforeach
                             </tbody>
                         </table>
                     </div>
@@ -108,5 +123,6 @@
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,10 @@ use Illuminate\Support\Facades\DB;
 
 Route::get('/', function (){
     $orders = DB::table('orders')->get();
+
     return view('index',compact('orders'));
 });
+
 Route :: get('about',function(){
     if(isset ($_GET['name']))
     $name=  $_GET['name'];
@@ -30,3 +32,31 @@ Route :: GET ('tasks',function(){
     'task3'];
     return view('Tasks',compact('Tasks'));
 });
+
+
+Route :: post('delete/{id}', function($id){
+    DB:: table('orders')->where ('id',$id)->delete();
+
+    return redirect()->back();
+});
+
+Route::post('editt/{id}',function($id){
+    $n = request('taskName');
+    if($n){
+    DB::table('orders')->where('id',$id)->update(['name'=>$n]);
+
+    return redirect('/');}
+    else
+    return view('edit',compact('id'));
+
+});
+
+Route::get('insert',function(){
+    DB::table('orders')->insert([
+        'name'=>$_GET['name'],
+        'created_at'=>now(),
+        'updated_at'=>now()
+    ]);
+    return redirect()->back();
+    });
+
